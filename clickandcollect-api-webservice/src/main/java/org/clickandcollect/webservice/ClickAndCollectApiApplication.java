38a66cab487c28146c1b3f0cce7dcf862ecd4d9a
alain.duguine@gmail.com <lRze3xLmxEXOqRfNzajg>
2020-05-20 15:36:45 +0200

    first commit, initializing project with spring boot maven multi-module application

---
+++ clickandcollect-api-webservice/src/main/java/org/clickandcollect/webservice/ClickAndCollectApiApplication.java
@@ -0,0 +1,13 @@
+package org.clickandcollect.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickAndCollectApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickAndCollectApiApplication.class, args);
+    }
+
+}
---
+++ clickandcollect-api-webservice/src/test/java/org/clickandcollect/webservice/ClickAndCollectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clickandcollect.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickAndCollectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
