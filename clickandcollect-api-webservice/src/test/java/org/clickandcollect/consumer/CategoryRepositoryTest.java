0e0eba94e63e13cfb76606436d2aae5bdaa540d5
alain.duguine@gmail.com <lRze3xLmxEXOqRfNzajg>
2020-05-26 14:51:57 +0200

    changing test configuration to fix CI build

--- clickandcollect-api-webservice/src/test/java/org/clickandcollect/consumer/CategoryRepositoryTest.java
+++ clickandcollect-api-webservice/src/test/java/org/clickandcollect/consumer/CategoryRepositoryTest.java
@@ -2,20 +2,16 @@ package org.clickandcollect.consumer;
 
 import org.clickandcollect.webservice.ClickAndCollectApiApplication;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = ClickAndCollectApiApplication.class)
+@ContextConfiguration(classes = {ClickAndCollectApiApplication.class})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@DataJpaTest()
+@SpringBootTest
 class CategoryRepositoryTest {
 
     @Autowired
