671b2055ae7439cf5812b8315d9c2bb877b0ef55
alain.duguine@gmail.com <lRze3xLmxEXOqRfNzajg>
2020-05-27 17:54:46 +0200

    Changed WebConfig class as Spring Bean in main class Added column length limit for product.name

--- clickandcollect-api-model/src/main/java/org/clickandcollect/model/entities/Product.java
+++ clickandcollect-api-model/src/main/java/org/clickandcollect/model/entities/Product.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -21,10 +20,9 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
+    @Column(unique = true, length = 100)
     @NotNull
     private String name;
-    @Column(length = 255)
     private String description;
     @Min(0)
     @NotNull
--- clickandcollect-api-webservice/src/main/java/org/clickandcollect/webservice/ClickAndCollectApiApplication.java
+++ clickandcollect-api-webservice/src/main/java/org/clickandcollect/webservice/ClickAndCollectApiApplication.java
@@ -3,8 +3,11 @@ package org.clickandcollect.webservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.clickandcollect"})
@@ -14,6 +17,16 @@ public class ClickAndCollectApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClickAndCollectApiApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
